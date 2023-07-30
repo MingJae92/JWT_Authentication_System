b23b86c7afc17f3a70f9ca4f66e88e6a955e951b
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-07-30 12:10:24 +0100

    Front end implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/LoginForm';
+import LoginIn from './components/SignUp';
+import MyDetails from './components/MyDetails';
+import SignUp from './components/SignUp'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Routes>
+        <Route path="/" element={<LoginForm/>}></Route>
+        <Route path="/SignUp" element={<SignUp/>}></Route>
+        <Route path="/MyDetails" element={<MyDetails/>}></Route>
+      </Routes>
+      <LoginForm/> 
+      <SignUp/>
+      <MyDetails/>
     </div>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,143 @@
+import React, { useState } from 'react';
+
+import {
+  TextField,
+  Button,
+  Container,
+  Typography,
+  Grid,
+  Box,
+} from '@mui/material';
+
+const LoginForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    // Perform login logic here, e.g., send the email and password to the server
+    console.log('Login - Email:', email, 'Password:', password);
+  };
+
+  const handleSignUp = () => {
+    // Perform sign-up logic here, e.g., send the name, email, and password to the server
+    console.log('Sign Up - Name:', name, 'Email:', email, 'Password:', password);
+  };
+
+  return (
+    <Container
+      maxWidth="sm"
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        width="300px"
+        py={3}
+        px={4}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <Typography variant="h4" gutterBottom>
+          Login
+        </Typography>
+        <form style={{ width: '100%' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                label="Name"
+                fullWidth
+                variant="outlined"
+                value={name}
+                onChange={handleNameChange}
+                required
+                size="small"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Email"
+                fullWidth
+                variant="outlined"
+                value={email}
+                onChange={handleEmailChange}
+                required
+                size="small"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Password"
+                fullWidth
+                variant="outlined"
+                value={password}
+                onChange={handlePasswordChange}
+                type="password"
+                required
+                size="small"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                onClick={handleLogin}
+              >
+                Login
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+      <Box mt={2} textAlign="center" width="100%">
+        <Typography variant="body2" component="div">
+          <Box  display="inline-block">
+            Already have an account? Login
+          </Box>
+        </Typography>
+      </Box>
+      <Box
+        position="absolute"
+        top={0}
+        right={0}
+        mt={3}
+        mr={3}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+       
+        <Box display="flex" justifyContent="center">
+      <Button color="primary" onClick={handleSignUp}>
+            Sign Up
+          </Button>
+          <Box ml={1}>
+            <Button color="primary" onClick={handleLogin}>
+              Login
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default LoginForm;
